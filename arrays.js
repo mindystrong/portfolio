2c4df0cc752ccd0b5b7f63190f8bdd4ae901f5c7
Mindy Strong <strong.mindy@outlook.com>
2018-10-22 14:30:14 -0600

    stuff

---
+++ arrays.js
@@ -0,0 +1,38 @@
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Home | CSA Box</title>
+  <meta name="viewport" content="width=device-width">
+  <meta name="author" content="Mindy Strong">
+  <meta name="description" content="Challenge #1">
+</head>
+
+<body>
+   
+  <header></header>
+  <main>
+
+  
+  </main>
+</body>
+
+
+<script>
+const dogs = ['chief', 'boogie', 'mochi', 'cactus'];
+const cats = ['vanilla', 'spot', 'meow'];
+
+
+function filter() {
+for (let i = 0; i < dogs.length, i++) {
+    if (dogs[i] == 'Chief') {
+        return dogs[i];
+    }
+}
+console.log(dogs);
+}
+</script>
+</html>
--- csa/fill-box.js
+++ csa/fill-box.js
@@ -1,147 +1,193 @@
-const vegetableList = [{
+const produceList = [{
         name: "onion",
         imgsrc: "/csa-images/onion-pic.png",
-        availability: ["summer"],
-        price: "2",
+        availability: ["summer", "winter", "fall"],
+        price: 2,
         markets: "",
     },
     {
         name: "corn",
         imgsrc: "/csa-images/corn-pic.png",
         availability: ["summer"],
-        price: "1",
+        price: 2,
         markets: ""
     },
     {
         name: "tomato",
         imgsrc: "/csa-images/tomato-pic.png",
         availability: ["summer"],
-        price: "1",
+        price: 1,
         markets: ""
     },
     {
         name: "spinach",
         imgsrc: "/csa-images/spinach-pic.png",
         availability: ["winter"],
-        price: "1",
+        price: 1,
         markets: ""
-    }
+    },
+    {
+        name: "beets"
+        imgsrc: 
+        availability: ["winter", "fall"]
+        price:
+        markets:""
+    },
+    {
+        name: "apricots"
+        imgsrc:
+        availability: ["spring"]
+        price:
+        markets:""
+    },
+    {
+        name: "grapefruit"
+        imgsrc:
+        availability: ["winter"]
+        price:
+        markets:""
+    },
+    {
+        name: "kiwi"
+        imgsrc:
+        availability: ["winter", "summer"]
+        price:
+        markets:""
+    },
+    {
+        name: "pumpkin"
+        imgsrc:
+        availability: ["winter", "fall"]
+        price:
+        markets:""
+    },
+    {
+        name: "broccoli"
+        imgsrc:
+        availability: ["spring", "fall"]
+        price:
+        markets:""
+    },
+    {
+        name: "avocado"
+        imgsrc:
+        availability: ["summer"]
+        price:
+        markets:""
+    },
+    {
+        name: "apple"
+        imgsrc:
+        availability: ["summer"]
+        price:
+        markets:""
+    },
+    {
+        name: "potato"
+        imgsrc:
+        availability: ["fall", "winter"]
+        price:
+        markets:""
+    },
+    {
+        name: "pear"
+        imgsrc:
+        availability: ["winter", "fall"]
+        markets:""
+    },
+    {
+        name: "kale"
+        imgsrc:
+        availability: ["fall"]
+        price:
+        markets:""
+    },
+    {
+        name:
+        imgsrc:
+        availability:
+        price:
+        markets:""
+    },
+    {
+        name:
+        imgsrc:
+        availability:
+        price:
+        markets:""
+    },
+    {
+        name:
+        imgsrc:
+        availability:
+        price:
+        markets:""
+    },
+    {
+        name:
+        imgsrc:
+        availability:
+        price:
+        markets:""
+    },
 ];
 
 let box = [];
+let cart = [];
 
-//sort through object and create option list of produce in season
-function createSummerOptions() {
+//sort through object and create buttons of produce in season
+function createOptions(season) {
+
     let selectlist = document.getElementById("list");
 
-    for (let i = 0; i < vegetableList.length; i++) {
-        if (vegetableList[i].availability == "summer") {
-            let name = vegetableList[i].name;
-            let image = vegetableList[i].imgsrc;
-            let option = document.createElement("option");
-            option.textContent = name;
-            option.value = name;
-            option.id = name;
-           
+    for (let i = 0; i < produceList.length; i++) {
+        if (produceList[i].availability.indexOf(season) > -1) {
+            let name = produceList[i].name;
+            let image = produceList[i].imgsrc;
+            let price = produceList[i].price;
+
+            let produceBtn = document.createElement("button");
+            produceBtn.textContent = name;
+            produceBtn.value = name;
+            produceBtn.id = name;
+            produceBtn.setAttribute("data-price", price);
 
             //create and set new img element and add to box onclick
-            option.onclick = function convert() {
+            produceBtn.onclick = function convert() {
+
                 let x = document.createElement("img");
                 x.setAttribute("src", image);
                 x.setAttribute("alt", name);
-                box.push(x);
+                
+                if (box.length < 8) {
+                    box.push(x);
+                    cart.push({price, name});
+                }
                 //display box in resultBox div
                 for (let j = 0; j < box.length; j++) {
                     document.getElementById('resultBox').appendChild(box[j]);
-                }
-            }
-            selectlist.appendChild(option);
-        }
-    }
+                
+                } 
+            }   
+            selectlist.appendChild(produceBtn);
+        }    
+    }   
 }
 
-function createWinterOptions() {
-    let selectlist = document.getElementById("list");
+//get sum of prices array
+function addSum() {
+    let sum = 0;
+    for (let k = 0; k < cart.length; k++) {
+        sum += parseInt(cart[k].price);   
+    }    
+    return sum; 
+}     
 
-    for (let i = 0; i < vegetableList.length; i++) {
-        if (vegetableList[i].availability == "winter") {
-            let name = vegetableList[i].name;
-            let image = vegetableList[i].imgsrc;
-            let option = document.createElement("option");
-            option.textContent = name;
-            option.value = name;
-            option.id = name;
-            //create and set new img element and add to box onclick
-            option.onclick = function convert() {
-                let x = document.createElement("img");
-                x.setAttribute("src", image);
-                x.setAttribute("alt", name);
-                box.push(x);
-                //display box in resultBox div
-                for (let j = 0; j < box.length; j++) {
-                    document.getElementById('resultBox').appendChild(box[j]);
-                }
-            }
-            selectlist.appendChild(option);
-        }
-    }
-}
+//window alert sum
+ function showPrice() {
+    let checkout = addSum();
+    window.alert(checkout);
 
-function createFallOptions() {
-    let selectlist = document.getElementById("list");
-
-    for (let i = 0; i < vegetableList.length; i++) {
-        if (vegetableList[i].availability == "fall") {
-            let name = vegetableList[i].name;
-            let image = vegetableList[i].imgsrc;
-            let option = document.createElement("option");
-            option.textContent = name;
-            option.value = name;
-            option.id = name;
-
-            //create and set new img element and add to box onclick
-            option.onclick = function convert() {
-                let x = document.createElement("img");
-                x.setAttribute("src", image);
-                x.setAttribute("alt", name);
-                box.push(x);
-                //display box in resultBox div
-                for (let j = 0; j < box.length; j++) {
-                    document.getElementById('resultBox').appendChild(box[j]);
-                }
-            }
-            selectlist.appendChild(option);
-        }
-    }
-}
-
-function createSpringOptions() {
-    let selectlist = document.getElementById("list");
-
-    for (let i = 0; i < vegetableList.length; i++) {
-        if (vegetableList[i].availability == "spring") {
-            let name = vegetableList[i].name;
-            let image = vegetableList[i].imgsrc;
-            let option = document.createElement("option");
-            option.inp
-            option.textContent = name;
-            option.value = name;
-            option.id = name;
-
-            //create and set new img element and add to box onclick
-            option.onclick = function convert() {
-                let x = document.createElement("img");
-                x.setAttribute("src", image);
-                x.setAttribute("alt", name);
-                box.push(x);
-                //display box in resultBox div
-                for (let j = 0; j < box.length; j++) {
-                    document.getElementById('resultBox').appendChild(box[j]);
-                }
-            }
-            selectlist.appendChild(option);
-        }
-    }
 }
 
 
