28b2493b1ef160c02c21f7e41585ff489a4bb5f3
Mindy Strong <strong.mindy@outlook.com>
2018-12-01 19:29:40 -0700

    dsad

--- gardening/gardening.js
+++ gardening/gardening.js
@@ -6,7 +6,7 @@ function loadJSON() {
   const request = new XMLHttpRequest();
   request.open('GET', url, true);
 
-  request.onload = function createZoneOptions() {
+  request.onload = function createZoneOptions(zoneFrost) {
 
     //parse data from JSON file
     if (request.status === 200) {
@@ -21,9 +21,11 @@ function loadJSON() {
         zoneOption.text = z;
         zoneOption.id = z;
         zoneDrop.appendChild(zoneOption);
-        //event listener to to create list of produce onchange
-        zoneDrop.addEventListener('change', createProduceOptions);
       }
+      //event listener to to create list of produce onchange
+      seasonList.addEventListener('change', createProduceOptions);
+      zoneDrop.addEventListener('change', createProduceOptions);
+
 
       function createProduceOptions() {
         //grab season and zone user selection values
@@ -48,18 +50,19 @@ function loadJSON() {
       let plantNow = document.getElementById('plantNowBtn');
       plantNow.addEventListener('click', renderProduceInfo);
 
-      function renderProduceInfo() {
+      function renderProduceInfo(plantFrost) {
         //get the value of the current produce user selection
         let currentProduce = document.getElementById('produceList').value;
         //match user selection with produce name
         for (let k = 0; k < data.produce.length; k++) {
           if (data.produce[k].name == currentProduce) {
+            plantFrost = data.produce[k].frost;
             //grab unordered list element from HTML
             let produceList = document.getElementById('plantResults');
             //create list item and display produce info
             const item = document.createElement('li');
             item.innerHTML = ` 
-            <h2>Plant in _____ days!</h2>
+            <h2>Plant in ${getDaysUntilPlant()} days!</h2>
             <p>Produce: ${data.produce[k].name}</p>
             <p>Environment: ${data.produce[k].environment}</p>
             <p>Sun Exposure: ${data.produce[k].sunExposure}</p>
@@ -67,11 +70,40 @@ function loadJSON() {
             <p>Soil pH: ${data.produce[k].soilpH}</p>
             `;
             produceList.appendChild(item);
-            
 
-            
           }
-          
+
+          function getDaysUntilPlant() {
+            //get current user zone selection
+            let currentZone = document.getElementById("zoneList").value;
+
+            //get today's day of year
+            var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+            var oneDay = 1000 * 60 * 60 * 24;
+            var today = Math.floor(diff / oneDay);
+
+            //match user zone selection with zones in JSON object
+            for (let z = 0; z < data.zones.length; z++) {
+              if (currentZone == data.zones[z].name) {
+
+                //if match, add the zone frost day of year to plant days from frost
+                let zoneFrost = data.zones[z].frost;
+                let plantDayOfYear = parseInt(zoneFrost) + parseInt(plantFrost);
+
+                //console checks
+                console.log(plantFrost);
+                console.log(zoneFrost);
+                console.log(plantDayOfYear);
+                console.log(today);
+
+                //subtract today from the plant day of year and return
+                let daysUntilPlant = parseInt(plantDayOfYear) - today;
+                return daysUntilPlant;
+              }
+            }
+          }
         }
       }
     }
@@ -81,30 +113,31 @@ function loadJSON() {
 
 
 
-//returns today's day of year from 1-365
-function getTodayDayOfYear() {
-  var now = new Date();
-  var start = new Date(now.getFullYear(), 0, 0);
-  var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
-  var oneDay = 1000 * 60 * 60 * 24;
-  var day = Math.floor(diff / oneDay);
-  return day;
-}
+// //returns today's day of year from 1-365
+// function getTodayDayOfYear() {
+//   var now = new Date();
+//   var start = new Date(now.getFullYear(), 0, 0);
+//   var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+//   var oneDay = 1000 * 60 * 60 * 24;
+//   var day = Math.floor(diff / oneDay);
+//   return day;
 
-//returns the day of year the produce should be planted
-function getPlantDayOfYear() {
-  // return (data.zone.frost + data.produce.frost);
-}
+// }
 
-//returns how many days until you can plant
-function getDaysUntilPlant() {
-  getTodayDayOfYear() = today;
-  getPlantDayOfYear() = plantDay;
-  if (plantDay < today) {
-    window.alert('sorry, plant date has passed');
-    return false;
-  } else {
-    let difference = plantDay - today;
-    return difference;
-  }
-}
+// //returns the day of year the produce should be planted
+// function getPlantDayOfYear() {
+//   // return (data.zone.frost + data.produce.frost);
+// }
+
+// //returns how many days until you can plant
+// function getDaysUntilPlant() {
+//   getTodayDayOfYear() = today;
+//   getPlantDayOfYear() = plantDay;
+//   if (plantDay < today) {
+//     window.alert('sorry, plant date has passed');
+//     return false;
+//   } else {
+//     let difference = plantDay - today;
+//     return difference;
+//   }
+//}
